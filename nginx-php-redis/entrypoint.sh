c3196d2942a92b732aee5fe23bcd89feb996c799
qinlh <qinlinhui@atai.net.cn>
2020-12-31 15:04:04 +0800

    未发给他

--- nginx-php-redis/entrypoint.sh
+++ nginx-php-redis/entrypoint.sh
@@ -3,19 +3,20 @@ set -e
 
 echo `service nginx status`
 echo '1.启动nginx....'
-/etc/init.d/nginx restart &
-sleep 3
+/etc/init.d/nginx restart
+# sleep 3
 echo `service nginx status`
-
-echo `service redis status`
-echo '3.启动redis....'
-/etc/init.d/redis restart &
-sleep 3
-echo `service redis status`
 
 echo `service php-fpm status`
 echo '2.启动php-fpm....'
 /etc/init.d/php-fpm restart
-sleep 3
+# sleep 3
 echo `service php-fpm status`
 
+echo `service redis status`
+echo '3.启动redis....'
+/etc/init.d/redis restart
+# sleep 3
+echo `service redis status`
+
+tail -f /dev/null
